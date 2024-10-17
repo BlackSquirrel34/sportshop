dc4807b287fddd979eb59ead4b22855675b89846
BlackSquirrel <Maexxla3458@proton.me>
2024-10-17 19:28:55 +0200

    navigation and cart finished

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 // combines the individual route modules so that they can be applied in a single step
 import { Express } from "express";
 import { createCatalogRoutes } from "./catalog";
+import { createCartMiddleware, createCartRoutes } from "./cart"
 
 export const createRoutes = (app: Express) => {
+    createCartMiddleware(app);
     createCatalogRoutes(app);
+    createCartRoutes(app);
 }
